47cdd61cf01f21e8912c727e3a053f5f24c7f23c
Amy Liu <maymayliu@maymayliu-mbp.dhcp.thefacebook.com>
2018-07-02 13:36:18 -0700

    Initial Commit

---
+++ thotter/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  thotter
+//
+//  Created by Amy Liu on 7/2/18.
+//  Copyright © 2018 Amy Liu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
